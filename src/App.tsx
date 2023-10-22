016b0ed94d934e88356bc9a3a56abaa8aa2e0439
Bastien WEBER <weber.bastien@gmail.com>
2023-10-22 18:30:13 +0200

    :sparkles: Add ReactPixi

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,35 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { KeyboardEvent, useState } from 'react';
 import './App.css';
+import { Sprite, Stage } from '@pixi/react';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [y, setY] = useState<number>(200);
+  const handleKeyDown = (e: KeyboardEvent) => {
+    console.log('zizi');
+    if (e.key === 'ArrowUp') {
+      setY(y - 10);
+    }
+
+    if (e.key === 'ArrowDown') {
+      setY(y + 10);
+    }
+
+    console.log(e.key);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h2>Splash Pong !</h2>
+      <Stage onKeyDown={handleKeyDown} tabIndex={0} width={800} height={600}>
+        {/** For test purposes, to be removed */}
+        <Sprite
+          image="https://pixijs.io/pixi-react/img/bunny.png"
+          x={10}
+          y={y}
+        />
+        {/***/}
+      </Stage>
+    </div>
   );
 }
 
